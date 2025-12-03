db45afbc8ee7171e3f517b427c799eb2d0813047
Ankit <dabhiankit38@gmail.com>
2025-12-03 09:48:43 +0530

    if

--- reactjs/src/decision_making.js
+++ reactjs/src/decision_making.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './matrix.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//functional component -> which returns JSX 
 
 function DigitalClock() {
-    //create date type object
+
     let today = new Date();
+
+    // TIME
     let hour = today.getHours();
     let minute = today.getMinutes();
     let seconds = today.getSeconds();
-    let ampm; 
-    if(hour>12)
-        ampm = "PM";
-    else 
-        ampm = "AM";
+    let ampm = hour >= 12 ? "PM" : "AM";
 
-    return (<>
-        <canvas id="matrix" />
-        <div className="clock-container">
-            <div>
-                <div className="matrix-clock" id="clock">
-                    {hour}:{minute}:{seconds} {ampm}
-                </div>
-                <div className="date-text" id="date">
-                    display date here 
+    // DATE FORMAT --> Tuesday, 02 December 2025
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    let dayName = days[today.getDay()];
+    let date = today.getDate().toString().padStart(2, "0");
+    let monthName = months[today.getMonth()];
+    let year = today.getFullYear();
+
+    let fullDate = `${dayName}, ${date} ${monthName} ${year}`;
+
+    return (
+        <>
+            <canvas id="matrix" />
+            <div className="clock-container">
+                <div>
+                    <div className="matrix-clock" id="clock">
+                        {hour}:{minute}:{seconds} {ampm}
+                    </div>
+                    <div className="date-text" id="date">
+                        {fullDate}
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-    )
+        </>
+    );
 }
+
 root.render(<DigitalClock />);
